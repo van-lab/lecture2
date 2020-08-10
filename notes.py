749151b0ff96b3112117dd3e3211328bfa49ffac
van-lab <65830784+van-lab@users.noreply.github.com>
2020-08-10 17:53:54 -0500

    add note with session

--- notes.py
+++ notes.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
-from flask_session.__init__ import Session
+from flask import Flask, render_template, request, session
+from flask_session import Session
+#from flask_session.__init__ import Session
 
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
@@ -10,10 +11,12 @@ notes = []
 
 @app.route("/", methods=["GET", "POST"])
 def noteindex():
+  if session.get("notes") is None:
+    session["notes"] = []
   if request.method == "POST":
     note = request.form.get("note")
-    notes.append(note)
-  return render_template("noteindex.html", notes=notes)
+    session["notes"].append(note)
+  return render_template("noteindex.html", notes=session["notes"])
 
 @app.route("/hello", methods=["GET", "POST"])
 def hello():
